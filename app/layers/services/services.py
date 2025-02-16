9aca5c0fcdbc256b3bf050ac2b490a74baeefac2
Cristian <criiisgonza@gmail.com>
2025-02-16 19:12:13 -0300

    trabajo terminado

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -31,7 +31,8 @@ def filterByCharacter(name):
 
     for card in getAllImages():
         # debe verificar si el name está contenido en el nombre de la card, antes de agregarlo al listado de filtered_cards.
-        filtered_cards.append(card)
+        if name.lower() in card.name.lower():
+            filtered_cards.append(card)
 
     return filtered_cards
 
@@ -41,7 +42,8 @@ def filterByHouse(house_name):
 
     for card in getAllImages():
         # debe verificar si la casa de la card coincide con la recibida por parámetro. Si es así, se añade al listado de filtered_cards.
-        filtered_cards.append(card)
+        if card.house.lower() == house_name.lower():
+            filtered_cards.append(card)
 
     return filtered_cards
 
